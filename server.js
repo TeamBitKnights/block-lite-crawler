173d76e8480f113898dacb7d5d511d5e515c2474
KAPUIST <91464689+KAPUIST@users.noreply.github.com>
2024-10-01 10:21:28 +0900

    Update:PORT

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = 3006;
-const { rpcCheckBlockNum } = require('./rpcCheckBlockNum')
-const { rpcRecheckBlockNum } = require('./rpcRecheckBlockNum')
 
-require('dotenv').config()
+const { rpcCheckBlockNum } = require('./rpcCheckBlockNum');
+const { rpcRecheckBlockNum } = require('./rpcRecheckBlockNum');
 
+require('dotenv').config();
+const port = process.env.PORT || 3006;
+console.log(process.env.PORT);
 // forever
-setInterval(() => rpcCheckBlockNum(), 2000) // Checking latest block data every 2 seconds
-setInterval(() => rpcRecheckBlockNum() , 60000) // Checking 30 previous block data every minute
+setInterval(() => rpcCheckBlockNum(), 2000); // Checking latest block data every 2 seconds
+setInterval(() => rpcRecheckBlockNum(), 60000); // Checking 30 previous block data every minute
 
-app.get("/", (req, res) =>{
-    res.json("crawling server")
-})
+app.get('/', (req, res) => {
+  res.json('crawling server');
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at ${port}`);
-})
+  console.log(`Example app listening at ${port}`);
+});
